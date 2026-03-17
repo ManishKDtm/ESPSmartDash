bbf77b573c826ce1e75902dd15cc55072cf673eb
Manish_KD <92296330+ManishKDtm@users.noreply.github.com>
2026-03-17 13:54:10 +0530

    Enhance SDWiFiManager with mode and IP methods
    Added WiFi mode setting and IP retrieval method.

--- src/core/WiFiManager.h
+++ src/core/WiFiManager.h
@@ -1,5 +1,6 @@
 #ifndef SD_WIFI_MANAGER_H
 #define SD_WIFI_MANAGER_H
+
 #pragma once
 #include <Arduino.h>
 
@@ -7,21 +8,25 @@
 #include <WiFi.h>
 #elif defined(ESP8266)
 #include <ESP8266WiFi.h>
+#else
+#error "ESPSmartDash supports ESP32 and ESP8266 only"
 #endif
 
 class SDWiFiManager {
-
 public:
 
-static void connect(const char* ssid,const char* pass){
+  static void connect(const char* ssid, const char* pass) {
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(ssid, pass);
 
-WiFi.begin(ssid,pass);
+    while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+    }
+  }
 
-while(WiFi.status()!=WL_CONNECTED){
-delay(500);
-}
-
-}
-
+  static IPAddress ip() {
+    return WiFi.localIP();
+  }
 };
+
 #endif
